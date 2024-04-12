0ca11958182f56e1861c6623af8980876f8f4f47
AnnaHtslk <hannah.htslk@gmail.com>
2024-04-12 14:15:58 +0300

    styled gallery

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,21 +3,36 @@
           `
  <li class="gallery-item">
   <a class="gallery-link" href="${picture.largeImageURL}">
+  <div class="image-wrapper">
     <img
       class="gallery-image"
       src="${picture.webformatURL}"
       alt="${picture.tags}"
     />
+     </div>
     </a>
     <div class="content">
-       <h3 class="title">Likes</h3>
+
+    <div class="detailed-info">
+    <h3 class="title">Likes</h3>
        <p class="quantity">${picture.likes}</p>
+         </div>
+
+         <div class="detailed-info">
        <h3 class="title">Views</h3>
        <p class="quantity">${picture.views}</p>
+        </div>
+      
+        <div class="detailed-info">
        <h3 class="title">Comments</h3>
        <p class="quantity">${picture.comments}</p>
+       </div>
+
+        <div class="detailed-info">
        <h3 class="title">Downloads</h3>
        <p class="quantity">${picture.downloads}</p>
+       </div>
+
     </div>
 </li> 
         `).join('');
